2a2b4dfa9dc3150c11d73f28da83948302fb40e3
ktbm <boliventtalla12@gmail.com>
2026-01-06 14:17:49 +0100

    update settings

--- tenant/tenant/settings.py
+++ tenant/tenant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8guwd64i8ptqyptp95m7t5$+0fh2wy5131@5r+1afely1uef@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

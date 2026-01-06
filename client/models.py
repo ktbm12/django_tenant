b24d74d68e17bbd7f0b25139feefe49593dbe0cb
ktbm <boliventtalla12@gmail.com>
2026-01-06 15:37:00 +0100

    update

--- client/models.py
+++ client/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django_tenants.models import TenantMixin, DomainMixin
 # Create your models here.
+class Client(TenantMixin):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+        class Domain(DomainMixin):
+            domain = models.CharField(max_length=255, unique=True)
+            tenant = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='domains')            
